926e808c2ddb6d8822f5480b3b98c0e86cdc83ed
aviiciii <laavesh1@gmail.com>
2022-09-08 15:44:47 +0530

    fixed path for articles

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'encyclopedia'
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:name>", views.article, name="article")
+    path("wiki/<str:name>", views.article, name="article")
 ]
